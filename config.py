9eaa352afa7a14c2c5b08b32676c54fe1ff338ee
dimassumantri <82698723+dimassumantri@users.noreply.github.com>
2021-04-19 06:21:04 +0700

    app id and passwd update

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("13ec83fe-eafc-4c6d-af38-ebecc88d6dab", "")
+    APP_PASSWORD = os.environ.get("~IDRY5x3Hn2b--l_bP9Lldk_6eS6PmI6P~", "")
